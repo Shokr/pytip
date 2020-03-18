aa7f3aa228537f4dda4cada42e65f91ec3dfef08
Muhammed Shokr <mohammedshokr2014@gmail.com>
2020-03-19 01:25:41 +0200

    CORE

--- pytip/core/models.py
+++ pytip/core/models.py
@@ -1,16 +1,24 @@
 from django.db import models
 from django.utils import timezone
 
+from pytip.users.models import User
+
 
 class Tweet(models.Model):
-    tweet = models.FloatField(db_column='IdTweet')
-    text = models.TextField(db_column='TweetText', blank=True, null=True)
+    tweet = models.FloatField(db_column='IdTweet', unique=True)
+    text = models.TextField(db_column='TweetText', max_length=280)
     created_at = models.DateTimeField(db_column='TimeCreated', default=timezone.now)
     re_tweet_count = models.BigIntegerField(db_column='RetweetCount', blank=True, null=True)
     favorite_count = models.BigIntegerField(db_column='FavoriteCount', blank=True, null=True)
     urls = models.TextField(db_column='Urls', blank=True, null=True)
     medias = models.TextField(db_column='Medias', blank=True, null=True)
     tags = models.TextField(db_column='Tags', blank=True, null=True)
+
+    publisher = models.ForeignKey(to=User, related_name='PublisherUser',
+                                  on_delete=models.CASCADE, db_column='IdPublisher', blank=True, null=True)
+    published = models.BooleanField(db_column='Published', default=False)
+
+    time_published = models.DateTimeField(db_column='TimePublished', blank=True, null=True)
 
     class Meta:
         managed = False
