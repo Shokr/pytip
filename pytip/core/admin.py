cdd7c80ec8aa2fdd901f167352eb4cf34c7d4c43
Muhammed Shokr <mohammedshokr2014@gmail.com>
2020-03-18 23:19:30 +0200

    CORE

--- pytip/core/admin.py
+++ pytip/core/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 
+from .froms import TweetForm
 from .models import *
 
-admin.site.register(Tweet)
+
+class TweetAdmin(admin.ModelAdmin):
+    form = TweetForm
+
+    search_fields = ('tweet', 'text')
+
+    list_display = (
+        'tweet',
+        'text',
+        're_tweet_count',
+        'favorite_count',
+        'created_at',
+    )
+
+    readonly_fields = ('created_at', 'favorite_count', 're_tweet_count')
+
+
+admin.site.register(Tweet, TweetAdmin)
