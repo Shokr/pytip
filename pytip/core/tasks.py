029eeac68f498206a72cdb629141d2ef241ddaf9
Muhammed Shokr <mohammedshokr2014@gmail.com>
2020-03-21 12:28:25 +0200

    CORE

--- pytip/core/tasks.py
+++ pytip/core/tasks.py
@@ -1,4 +1,5 @@
 import re
+from time import sleep
 
 import tweepy
 from celery.schedules import crontab
@@ -7,8 +8,7 @@ from celery.task import periodic_task
 from pytip.core.models import Tweet
 
 
-@periodic_task(run_every=(crontab(minute='*/15')), name="sync_tweets")
-# @shared_task
+@periodic_task(run_every=(crontab(minute='*/15')), name="sync_tweets", ignore_result=True)
 def sync_tweets():
     auth = tweepy.OAuthHandler('P4L0JIFEH8abGpiDCJZZfNwxJ', 'HGJS3zbqH57S5Blqpm6wOe4sUoZnaBOpeKttrcb9FTUu8jGU0H')
     auth.set_access_token('2716340250-mbZpFIuiz2uGWMm7sjbcM1XOz70abbT2RbVhWNb',
@@ -20,28 +20,33 @@ def sync_tweets():
 
     for tweet in results:
 
-        urls = tweet.entities['urls']
-        hashtags = tweet.entities['hashtags']
-
-        if 'media' in tweet.entities:
-            medias = tweet.entities['media']
-        else:
-            medias = ''
-
-        text = re.sub(r'&gt;', '>', tweet.text, flags=re.MULTILINE)
-
-        print(tweet)
-
-        if Tweet.objects.filter(tweet=tweet.tweet).exists():
+        if Tweet.objects.filter(tweet=tweet.id).exists() is True:
             print('Updating..')
-            Tweet.objects.filter(tweet=tweet.tweet).update(re_tweet_count=tweet.retweet_count,
-                                                           favorite_count=tweet.favorite_count)
-            print(tweet.id)
-        else:
-            print('Insert..')
-            Tweet.save(tweet=tweet.id, text=text, created_at=tweet.created_at,
-                       re_tweet_count=tweet.retweet_count, favorite_count=tweet.favorite_count,
-                       urls=urls, medias=medias, tags=hashtags)
             print(tweet.id)
 
-# sync_tweets
+            Tweet.objects.filter(tweet=tweet.id).update(re_tweet_count=tweet.retweet_count,
+                                                        favorite_count=tweet.favorite_count)
+        else:
+
+            urls = tweet.entities['urls']
+            hashtags = tweet.entities['hashtags']
+
+            if 'media' in tweet.entities:
+                medias = tweet.entities['media']
+            else:
+                medias = ''
+
+            text = re.sub(r'&gt;', '>', tweet.text, flags=re.MULTILINE)
+
+            print('Insert..')
+            print(tweet.id)
+
+            tweet = Tweet.objects.create(tweet=tweet.id, text=text, created_at=tweet.created_at,
+                                         re_tweet_count=tweet.retweet_count, favorite_count=tweet.favorite_count,
+                                         urls=urls, medias=medias, tags=hashtags)
+            tweet.save()
+
+            sleep(5)
+
+
+sync_tweets()
